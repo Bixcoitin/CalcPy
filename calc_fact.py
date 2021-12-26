4d3120609c761e440912c7c5cdfd2814d40f2405
Bixcoitin <celsoemilioleite@gmail.com>
2021-12-26 00:24:43 -0300

    CalcPy created, factories defined and classes created

---
+++ calc_fact.py
@@ -0,0 +1,15 @@
+#Need to import some liberies
+import tkinter as tk
+
+def make_root() -> tk.Tk:
+    root = tk.Tk()
+    root.title('CalcPy')
+    root.config(padx=10, pady=10, background='#fff')
+    root.resizable(False, False)
+    return root
+
+def make_label(root) -> tk.Label:
+    label = tk.Label(
+        root, text='Sem conta ainda',
+        anchor='e', justify='right', background='#fff'
+    )
