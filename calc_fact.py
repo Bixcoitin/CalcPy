8d576e9b04837bb32d9361be24af3e13ee10095b
Bixcoitin <celsoemilioleite@gmail.com>
2021-12-26 00:58:52 -0300

    created def display and def buttons

--- calc_fact.py
+++ calc_fact.py
@@ -1,5 +1,6 @@
 #Need to import some liberies
 import tkinter as tk
+from typing import List
 
 def make_root() -> tk.Tk:
     root = tk.Tk()
@@ -13,3 +14,41 @@ def make_label(root) -> tk.Label:
         root, text='Sem conta ainda',
         anchor='e', justify='right', background='#fff'
     )
+    label.grid(row=0, column=0, columnspan=5, sticky='news')
+    return label
+
+def make_display(root) -> tk.Entry:
+    display = tk.Entry(root)
+    display.grid(row=1, column=0, columnspan=5, sticky='news', pady=(0, 10))
+    display.config(
+        font=('Helvetica', 40, 'bold'),
+        justify='right', bd=1, relief='flat',
+        highlightthickness=1, highlightcolor='#ccc'
+    )
+    display.bind('<Control-a>', display_control_a)
+    return display
+
+def display_control_a(event):
+    event.widget.select_range(0, 'end')
+    event.widget.icursor('end')
+    return 'break'
+
+def make_buttons(root) -> List[List[tk.Button]]:
+    buttons_texts: List[List[str]] = [
+        ['7', '8', '9', '+', 'C'],
+        ['4', '5', '6', '-', '/'],
+        ['1', '2', '3', '*', '^'],
+        ['0', '.', '(', ')', '='],
+    ]
+
+    buttons: List[List[tk.Button]] = []
+
+    for row, row_value in enumerate(buttons_texts, start=2):
+        button_row = []
+        for col_index, col_value in enumerate(row_value):
+            btn = tk.Button(root, text=col_value)
+            btn.grid(row=row, column=col_index, sticky='news', padx=5, pady=5)
+            btn.config(
+                font=('Helvetica', 15, 'normal')
+                pady=40, width=1
+            )
